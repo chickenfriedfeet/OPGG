f6ddffd7883fddac08bf12da5d2c514a126d3672
chickenfriedfeet <krjoe227@gmail.com>
2023-09-22 01:27:16 +1200

    with '#' will be ignored, and an empty message aborts the commit Final Product

--- route.py
+++ route.py
@@ -2,23 +2,25 @@ from flask import Flask, render_template,request,redirect
 import sqlite3
 
 app = Flask(__name__)
-
+#directs to home page
 @app.route('/')
 def home():
     return render_template("home.html")
-
+#directs to contact page
 @app.route('/contact')
 def contact():
    return render_template("contact.html")
-
+#directs to about page
 @app.route('/about')
 def about():
    return render_template("about.html")
 
+#directs to all_champion page
 @app.route('/all_champions')
 def all_champions():
    conn = sqlite3.connect('OPGG.db')
    cur = conn.cursor()
+#selects all champions from the database
    cur.execute('SELECT * FROM Champions')
    results = cur.fetchall()
    return render_template('all_champions.html', results=results)
@@ -27,36 +29,36 @@ def all_champions():
 def chatbox():
     conn = sqlite3.connect('OPGG.db')
     cur = conn.cursor()
-
+#select info from chatbox database
     cur.execute("SELECT message_id, name, message_content FROM Chatbox")
     messages = cur.fetchall()
 
     conn.close()
     return render_template('chatbox.html', messages=messages)
 
-# Endpoint to receive new messages
 @app.route('/send_message', methods=['POST'])
 def send_message():
     name = request.form.get('name')
     message_content = request.form.get('message_content')
     conn = sqlite3.connect('OPGG.db')
     cur = conn.cursor()
-
-    # Insert the new message into the database
     cur.execute("INSERT INTO Chatbox (name, message_content) VALUES (?, ?)", (name, message_content))
     conn.commit()
     conn.close()
     
     return redirect('/chatbox')
 
+#directs to all_item page
 @app.route('/all_items')
 def all_items():
    conn = sqlite3.connect('OPGG.db')
    cur = conn.cursor()
+#selects all items from the database
    cur.execute('SELECT * FROM Items')
    results = cur.fetchall()
    return render_template('all_items.html', itemdata=results)
 
+#item page in items/id format
 @app.route('/items/<id>')
 def viewitem(id):
    conn=sqlite3.connect('OPGG.db')
@@ -64,7 +66,7 @@ def viewitem(id):
    cur.execute("SELECT * FROM Items WHERE item_id=?",(id,))
    itemdata=cur.fetchone()
    return render_template("items.html",itemdata=itemdata)
-
+#champion page in champion/id format
 @app.route('/champion/<id>')
 def viewchampion(id):
    conn=sqlite3.connect('OPGG.db')
